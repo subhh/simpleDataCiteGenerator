88a7ac4b3511defce1ff3067371eb1360524392f
Jan Frederik Maas <jfmaas@gmx.de>
2018-03-18 01:57:07 +0100

    First steps on implementing SAX parsing in HOS parser

--- src/datacitegenerator/parser/HosAggregatorParser.java
+++ src/datacitegenerator/parser/HosAggregatorParser.java
@@ -5,24 +5,112 @@
  */
 package datacitegenerator.parser;
 
+import datacitegenerator.DataCiteRecord;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.Attribute;
+import javax.xml.stream.events.Characters;
+import javax.xml.stream.events.EndElement;
+import javax.xml.stream.events.StartElement;
+import javax.xml.stream.events.XMLEvent;
+
 /**
  *
  * @author jfmaas
  */
 public class HosAggregatorParser extends DataCiteGeneratorParser {
     
-    @Override
-    public void parse() {
-        
+    LinkedList<DataCiteRecord> myRecords = new LinkedList<>();
+
+    public HosAggregatorParser() {
+        inputfile = null;
+        outputfile = null;
     }
     
+    @Override
+    public void parse() {
+
+        DataCiteRecord record = null;
+        
+        String context = "";
+        boolean in_context = false;
+      
+        try {
+            XMLInputFactory factory = XMLInputFactory.newInstance();
+            XMLEventReader eventReader =
+            factory.createXMLEventReader(new FileReader(inputfile));
+
+            while(eventReader.hasNext()) {
+                XMLEvent event = eventReader.nextEvent();
+               
+                switch(event.getEventType()) {
+               
+                case XMLStreamConstants.START_ELEMENT:
+                    StartElement startElement = event.asStartElement();
+                    String qName = startElement.getName().getLocalPart();
+                    
+                    // doc beginning? create new record
+                    if(qName.equalsIgnoreCase("doc")) { record = new DataCiteRecord();}
+                    
+                    if (qName.equalsIgnoreCase("field")) { 
+                        Iterator<Attribute> attributes = startElement.getAttributes();
+                        context = attributes.next().getValue();
+                        in_context = true;
+                    }
+
+                break;
+
+                case XMLStreamConstants.CHARACTERS:
+                    Characters characters = event.asCharacters();
+                    String content =  characters.getData();
+                    
+                    if(in_context) { 
+                        in_context = false;
+                        
+                        // do nothing with collections
+                        if (context.equalsIgnoreCase("collection")) {};
+                        
+                        // store identifier directly
+                        if (context.equalsIgnoreCase("identifier")) {
+                            
+                        }
+                        
+                    }
+                    
+                    
+
+                break;
+
+                case XMLStreamConstants.END_ELEMENT:
+                    EndElement endElement = event.asEndElement();
+                    
+                    // Doc ending? store record
+                    if(endElement.getName().getLocalPart().equalsIgnoreCase("doc")) {
+                        myRecords.add(record);
+                    }
+                break;
+            } 
+         }
+      } catch (FileNotFoundException e) {
+         e.printStackTrace();
+      } catch (XMLStreamException e) {
+         e.printStackTrace();
+      }
+   }
+
     @Override
     public String validate() {
         String result = "";
         
         return result;
-    }
-    
+    }    
 }
 
 
@@ -167,4 +255,26 @@ public class HosAggregatorParser extends DataCiteGeneratorParser {
         "stored":true,
          "indexed":"true"
 }
+
+
+
+<add>
+  <doc>
+    <field name="authors">Patrick Eagar</field>
+    <field name="subject">Sports</field>
+    <field name="dd">796.35</field>
+    <field name="numpages">128</field>
+    <field name="desc"></field>
+    <field name="price">12.40</field>
+    <field name="title">Summer of the all-rounder: Test and championship cricket in England 1982</field>
+    <field name="isbn">0002166313</field>
+    <field name="yearpub">1982</field>
+    <field name="publisher">Collins</field>
+  </doc>
+  <doc>
+  ...
+  </doc>
+</add>
+
+
 */
